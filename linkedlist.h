78bf3fa24b619c47877bdcded808023c4e91eb48
elekeller <115349982+elekeller@users.noreply.github.com>
2023-01-02 15:33:14 +0100

    Create linkedlist.h

---
+++ linkedlist.h
@@ -0,0 +1,40 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <pthread.h>
+
+/**
+ * @brief node points to data, next, prev and a lock
+ */
+
+typedef struct _node_
+{
+    pthread_mutex_t *lock;
+    struct node *prev;
+    struct node *next;
+    void *data;
+} node;
+
+void node_new(node *, int); // data = malloc(size);
+
+node *node_del(node *); // free(data); return next;
+
+typedef struct
+{
+    node *head;
+    node *tail;
+    int listsize;
+    int elementsize;
+} linkedlist;
+
+void ll_init(linkedlist *, int); // "Konstruktor"
+
+int ll_add(linkedlist *, void *); // node_new(new, l->elementsize); tail->prev->next = new; tail->prev = new;
+
+int ll_size(linkedlist *); // return l->listsize;
+
+void ll_dispose(linkedlist *); // free(all nodes)
+
+//
+
+#endif
